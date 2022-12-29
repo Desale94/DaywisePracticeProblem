55b42926f4fc1b47861e7f5b18569fe00078c2b4
Desale94 <121386567+Desale94@users.noreply.github.com>
2022-12-29 19:17:44 +0530

    Update PrimeNumber.sh

--- Day6/assignment/PrimeNumber.sh
+++ Day6/assignment/PrimeNumber.sh
@@ -9,18 +9,3 @@ do
   fi
 done
 echo "$n is a prime number."
-======================================================================
-function prime() {
-read -p "Enter the number: " n
-for((i=2; i<=num/2; i++))
-do
-      if [[ $n%$i -eq 0 ]]
-  then
-    echo "$n is not a prime number."
-    exit
-  fi
-done
-echo "$n is a prime number"
-}
-VALUE=$(prime)
-echo $VALUE
